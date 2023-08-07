434e9b3408abfb5a9a9a3fe7348db7aa219a1c62
Maddox <99859313+SvedishMafia@users.noreply.github.com>
2023-08-07 16:37:32 -0600

    Add files via upload

--- Heros/Bogart/Body.gd
+++ Heros/Bogart/Body.gd
@@ -1,38 +1,53 @@
 extends CharacterBody2D
 
+@onready var _animated_sprite = $AnimatedSprite2D
 var health = 10
-
+var right = true
 @export var speed = 40
+const DIRECTION_RIGHT = 1
+const DIRECTION_LEFT = -1
+var direction = Vector2(DIRECTION_RIGHT, 1)
 
-var target = position
+func set_direction(hor_direction):
+	if hor_direction == 0:
+		hor_direction = DIRECTION_RIGHT # default to right if param is 0
+	var hor_dir_mod = hor_direction / abs(hor_direction) # get unit direction
+	apply_scale(Vector2(hor_dir_mod * direction.x, 1)) # flip
+	direction = Vector2(hor_dir_mod, direction.y) # update direction
 
-func _input(event):
-	if event.is_action_pressed("click"):
-		target = get_global_mouse_position()
+func get_input():
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	velocity = input_direction * speed
+	if Input.is_action_pressed("right"):
+		_animated_sprite.play("walkright")
+		set_direction(DIRECTION_RIGHT)
+	elif Input.is_action_pressed("left"):
+			_animated_sprite.play("walkright")
+			set_direction(DIRECTION_LEFT)
+	elif Input.is_action_pressed("up"):
+			_animated_sprite.play("walkright")
+	elif Input.is_action_pressed("down"):
+			_animated_sprite.play("walkright")
+	else:
+			_animated_sprite.stop()
 
 func _physics_process(delta):
-	velocity = position.direction_to(target) * speed
-	# look_at(target)
-	if position.distance_to(target) > 10:
-		move_and_slide()
+	get_input()
+	move_and_slide()
 	update_health()
-	print(health)
-
-
 
 func update_health():
-	var healthbar = $healthbar
-	healthbar.value = health
+	var healthBar = $health_bar
+	healthBar.value = health
 	
 	if health >= 100:
-		healthbar.visible = false
+		healthBar.visible = false
 	else:
-		healthbar.visible = true
-	
-	
+		healthBar.visible = true
+
 func _on_regen_timer_timeout():
 	if health < 100:
-		health = health + 20
+		health += 2.5
 		if health > 100:
 			health = 100
 	if health <= 0:
--- Heros/Bogart/Bogart2.gd
+++ Heros/Bogart/Bogart2.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+var health = 10
 
 @export var speed = 40
 
@@ -14,3 +15,21 @@ func _physics_process(delta):
 	# look_at(target)
 	if position.distance_to(target) > 10:
 		move_and_slide()
+	update_health()
+
+func update_health():
+	var healthBar = $health_bar
+	healthBar.value = health
+	
+	if health >= 100:
+		healthBar.visible = false
+	else:
+		healthBar.visible = true
+
+func _on_regen_timer_timeout():
+	if health < 100:
+		health += 2.5
+		if health > 100:
+			health = 100
+	if health <= 0:
+		health = 0
