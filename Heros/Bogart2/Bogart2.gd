b24d8b560df5934f748f337224970024f2b1b02c
Aviatoraven <jontesteiner@gmail.com>
2023-08-15 14:32:54 -0700

    Put Bogart2 in a new folder

---
+++ Heros/Bogart2/Bogart2.gd
@@ -0,0 +1,38 @@
+extends CharacterBody2D
+
+var health = 10
+
+@export var speed = 40
+
+var target = position
+
+func _input(event):
+	if event.is_action_pressed("click"):
+		target = get_global_mouse_position()
+
+func _physics_process(delta):
+	velocity = position.direction_to(target) * speed
+	# look_at(target)
+	if position.distance_to(target) > 10:
+		move_and_slide()
+	update_health()
+	print(health)
+
+
+func update_health():
+	var healthbar = $healthbar
+	healthbar.value = health
+	
+	if health >= 100:
+		healthbar.visible = false
+	else:
+		healthbar.visible = true
+	
+	
+func _on_regen_timer_timeout():
+	if health < 100:
+		health += 5
+		if health > 100:
+			health = 100
+	if health <= 0:
+		health = 0
